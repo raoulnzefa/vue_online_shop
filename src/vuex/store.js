8b55d4f2d4afc3e04998350cf951399218906b52
Aleksandr <Sashkakonowawka4@gmail.com>
2021-09-19 00:02:23 +0300

    lesson 4. vuex + получение данных из db.json по api

---
+++ src/vuex/store.js
@@ -0,0 +1,38 @@
+import axios from 'axios';
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex);
+
+let store = new Vuex.Store({
+    state: { // тут хранятся все данные, переменные, объекты и т.д.
+        products: []
+    },
+    mutations: { // с помощью мутаций меняем данные состояний в state. СИНХРОННЫ. ОДИН В ОДИН МОМЕНТ.
+        SET_PRODUCTS_TO_STATE: (state, products) => {
+            state.products = products;
+        }
+    },
+    actions: { // ЭКШНЫ(методы). АСИНХРОННЫ, МНОГО В ОДИН МОМЕНТ.
+        GET_PRODUCTS_FROM_API({commit}) {
+            return axios('http://localhost:3000/products', {
+                method: "GET"
+            })
+            .then((products) => {
+                commit('SET_PRODUCTS_TO_STATE', products.data);
+                return products;
+            })
+            .catch((error) => {
+                console.log(error)
+                return error;
+            })
+        }
+    },
+    getters: { // даёт возможность получить данные из state
+        PRODUCTS(state) {
+            return state.products;
+        }
+    }
+})
+
+export default store;
